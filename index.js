b70dcdd0004a2fe6a7e088194c696ab17fbcd1a5
Jack Wearden <jack@jackwearden.co.uk>
2013-08-07 21:06:25 +0100

    send websocket message on new resource creation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var app = require('express')(),
+var expr = require('express'),
+    app = expr(),
     congo = require('congo'),
     io = require('socket.io').listen(app.listen(3000)),
+    bodyParser = expr.bodyParser(),
     resources = ['users', 'messages'];
 
 congo.configure({
@@ -12,24 +14,38 @@ congo.configure({
 function hasResource(req, res, next) {
   var resource = req.params.resource;
   if (resources.indexOf(resource) === -1) {
-    res.status(404).send('Don\'t have '+resource+' in '+JSON.stringify(resources));
-    next('routes');
+    res.send(400, {
+      statusCode: 400,
+      message: "That is not an available API resource"
+    });
   } else {
     next();
   }
 }
 
-app.get('/', function (req, res) {
-  res.sendfile('./index.html');
-});
+function validId(req, res, next) {
+  var id = req.params.id;
+  if (/^[0-9a-f]+$/.test(id) && id.length === 24) {
+    next();
+  } else {
+    res.send(400, {
+      statusCode: 400,
+      message: "ID should be a 24-digit hex value"
+    });
+  }
+}
 
-app.get('/api', function (req, res) {
-  res.send('hi');
-});
+congo.get(function (err, db) {
+  app.get('/', function (req, res) {
+    res.sendfile('./index.html');
+  });
 
-app.get('/api/:resource', hasResource, function (req, res) {
-  var resource = req.params.resource;
-  congo.get(function (err, db) {
+  app.get('/api', function (req, res) {
+    res.send('hi');
+  });
+
+  app.get('/api/:resource', hasResource, function (req, res) {
+    var resource = req.params.resource;
     db[resource].findAll({}, function (err, collection) {
       res.type('json').send({
         meta: {
@@ -40,23 +56,27 @@ app.get('/api/:resource', hasResource, function (req, res) {
       });
     });
   });
-});
 
-app.get('/api/:resource/:id', hasResource, function (req, res) {
+  app.get('/api/:resource/:id', hasResource, validId, function (req, res) {
   var resource = req.params.resource,
-      id = req.params.id;
-  congo.get(function (err, db) {
-    db[resource].findOne({id: id}, function (err, result) {
+      id = congo.ObjectID(req.params.id);
+    db[resource].findOne({_id: id}, function (err, result) {
       var count = Number(null !== result);
-      res.status([
-        404, 200
-      ][count]).type('json').send({
+      res.type('json').send([404, 200][count], {
         meta: {
           size: count,
           result: resource
         },
         data: (result || {})
       });
+    });
+  });
+
+  app.post('/api/:resource', hasResource, bodyParser, function (req, res) {
+    var resource = req.params.resource;
+    db[resource].insert(req.body, function (err, doc) {
+      io.sockets.emit('new'+resource, doc[0]);
+      res.location('/api/'+resource+'/'+doc['_id']).send(201, doc);
     });
   });
 });
